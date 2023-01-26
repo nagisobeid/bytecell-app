34f059a1096b9dc97ffdd068ef5abf502b00b8ac
nagiobeid <nagisobeid@gmail.com>
2023-01-26 05:13:45 +0000

    stuff

--- helpers.js
+++ helpers.js
@@ -10,6 +10,23 @@ const { default: Shopify } = require('@shopify/shopify-api');
 const { response } = require('express');
 
 
+
+const getDateTime = function() {
+	let date_ob = new Date();
+	let day = ("0" + date_ob.getDate()).slice(-2);
+	let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+	let year = date_ob.getFullYear();
+   
+	let date = year + "-" + month + "-" + day;
+    
+	let hours = date_ob.getHours();
+	let minutes = date_ob.getMinutes();
+	let seconds = date_ob.getSeconds();
+  
+	let dateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+	return dateTime;	
+}
+
 // This function returns the data object in the proper Shopify 'product' format
 const prepareProductFromDB = async function( prod ) {
     //console.log( prod[0] )
@@ -499,5 +516,6 @@ async function testing() {
 
 module.exports = {
     prepareProductFromDB,
-    parseSpecs
+    parseSpecs,
+    getDateTime
 }
--- test_app.js
+++ test_app.js
@@ -2,18 +2,17 @@ const actionsVariants = require( './actions/variants' )
 const helpers = require( './helpers' )
 
 const start = async () => {
-    console.log( '*********** TRANSACTION BEGIN **************' )
+    console.log( '*********** TRANSACTION BEGIN **************\n' )
     console.log( helpers.getDateTime() )
-    console.log( '\n' )
     try {
-        const products = await actionsVariants.updateVariantPrices()
-        const response = await actionsVariants.resetPriceChanged( { json : JSON.stringify(products) })
-        console.log( response )
+        //const products = await actionsVariants.updateVariantPrices()
+        //const response = await actionsVariants.resetPriceChanged( { json : JSON.stringify(products) })
+        console.log( 'task here' )
     } catch (error) {
         console.log( error )
     }
-    console.log( '\n' )
     console.log( helpers.getDateTime() )
+    console.log( '\n' )
     console.log( '*********** TRANSACTION END **************\n' )
 }
 

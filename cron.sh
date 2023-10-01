2d86535a1b58a15932a58555fd101c0ff82f53cc
nagiobeid <nagisobeid@gmail.com>
2023-10-01 23:36:08 +0000

    changed

--- cron.sh
+++ cron.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd /home/ec2-user/src/bytecell-app
-~/.nvm/versions/node/v17.6.0/bin/node app.js >> /home/ec2-user/src/bytecell-app/log
+
+if (ps aux | grep app.js | grep -v grep > /dev/null )
+	then
+      		echo app.js is already running, skipping execution >> /home/ec2-user/src/bytecell-app/log
+  	else
+		cd /home/ec2-user/src/bytecell-app
+		~/.nvm/versions/node/v17.6.0/bin/node app.js >> /home/ec2-user/src/bytecell-app/log
+ fi
